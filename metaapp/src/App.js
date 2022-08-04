7d2a67d256efb8a816274dc8173cfcd4352ce004
Matteo Cabrera <hyukaruu@hyukaruu.com>
2022-08-04 18:28:41 -0500

    instalacion tailwind

--- metaapp/src/App.js
+++ metaapp/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Encabezamiento from './componentes/Encabezamiento';
+import Pie from './componentes/Pie';
+import Principal from './componentes/Principal';
 
 function App() {
   return (
     <div className="App">
-      
+      <Encabezamiento></Encabezamiento>
+      <Principal></Principal>
+      <Pie></Pie>
     </div>
   );
 }
---
+++ metaapp/src/componentes/Pie.js
@@ -0,0 +1,9 @@
+function Pie() {
+    return (
+      <footer className="text-red-500">
+        <small>Desarrollado y diseñado por Matteo Cabrera, ©2022. Todos los derechos reservados </small>
+      </footer>
+    );
+  }
+  
+  export default Pie;
